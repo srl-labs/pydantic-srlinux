60e15f02efb8fd08a9fbe2071b22ef683a60b617
Roman Dodin <dodin.roman@gmail.com>
2025-01-28 11:17:59 +0100

    do not use aliases for schemas

--- generate_models_schema.py
+++ generate_models_schema.py
@@ -2,8 +2,8 @@ import json
 
 from pydantic_srlinux.models import acl, interfaces
 
-acl_schema = acl.Model().model_json_schema()
+acl_schema = acl.Model().model_json_schema(by_alias=False)
 json.dump(acl_schema, open("./schemas/acl_schema.json", "w"), indent=2)
 
-interfaces_schema = interfaces.Model().model_json_schema()
+interfaces_schema = interfaces.Model().model_json_schema(by_alias=False)
 json.dump(interfaces_schema, open("./schemas/interfaces_schema.json", "w"), indent=2)
